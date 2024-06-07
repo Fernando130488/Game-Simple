43c02e73e0a16cdd6f58b8b321290783536daac2
Fernando130488 <nandobritoferreira@hotmail.com>
2024-06-07 10:42:06 -0300

    adição do click como evento

--- script.js
+++ script.js
@@ -28,4 +28,7 @@ const loop = setInterval(() => {
     }
 
 }, 10);
-document.addEventListener('keydown', jump); 
+
+document.addEventListener('keydown', jump);
+document.addEventListener('click', jump);
+//document.addEventListener('touchstart', jump); 
