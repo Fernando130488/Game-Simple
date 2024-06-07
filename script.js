230d71bee616c209733b184f3f83e618b0485097
Fernando130488 <nandobritoferreira@hotmail.com>
2024-06-07 10:49:02 -0300

    alteração de evento: remoção do "click" e adição do "touchstart"

--- script.js
+++ script.js
@@ -30,5 +30,4 @@ const loop = setInterval(() => {
 }, 10);
 
 document.addEventListener('keydown', jump);
-document.addEventListener('click', jump);
-//document.addEventListener('touchstart', jump); 
+document.addEventListener('touchstart', jump); 
